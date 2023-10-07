40642604c9994b3888322952933b6c4ebb8abea9
Benton60 <145625002+Benton60@users.noreply.github.com>
2023-10-06 20:27:59 -0400

    Added all the files

---
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -0,0 +1,26 @@
+package com.example.myapplication
+
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val btnBigBoi = findViewById<Button>(R.id.btnBigBoi)
+        btnBigBoi.setOnClickListener {
+            switchButton()
+        }
+    }
+    fun switchButton(){
+        val textView = findViewById<TextView>(R.id.textView)
+        if(textView.text.toString() == "bois"){
+            textView.text = "YUUUUUHHHH"
+        }else{
+            textView.text = "bois"
+        }
+    }
+
+}
