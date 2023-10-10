384673a54b3a667f8b5f6d5bddce85702cfeb191
Benton60 <145625002+Benton60@users.noreply.github.com>
2023-10-10 19:41:11 -0400

    finished the save and load functions. created and finished the deletenote function

--- app/src/main/java/com/example/myapplication/OpenNoteActivity.kt
+++ app/src/main/java/com/example/myapplication/OpenNoteActivity.kt
@@ -13,47 +13,64 @@ class OpenNoteActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_note)
+        val btnSave = findViewById<Button>(R.id.btnSave)
+        val btnDelete = findViewById<Button>(R.id.btnDelete)
 
         setText(loadThisNote("file.txt"))
-        val btnBack = findViewById<Button>(R.id.btnBack)
-        btnBack.setOnClickListener {
-            if(saveThisNote("file")){
+
+        btnSave.setOnClickListener {
+            if(saveThisNote("file.txt")){
                 finish()
+            }else{
+                setText("Failed")
             }
+        }
+
+        btnDelete.setOnClickListener {
+            deleteThisNote("file.txt")
             finish()
         }
     }
-    fun getText(): String {
+    fun getNoteName(){
+
+    }
+    private fun getText(): String {
         val edtNote = findViewById<EditText>(R.id.edtNote)
         return edtNote.text.toString()
     }
-
-    fun setText(note: String?){
+    private fun setText(note: String?){
         val edtNote = findViewById<EditText>(R.id.edtNote)
         edtNote.setText(note, TextView.BufferType.EDITABLE)
     }
-
     private fun saveThisNote(noteName: String): Boolean{
         return try{
-            openFileOutput("$noteName.txt", MODE_PRIVATE).use {
-                val outputWriter = OutputStreamWriter(it)
-                outputWriter.write(getText())
-            }
+            val fileOutputStream = openFileOutput(noteName, MODE_PRIVATE)
+            val outputWriter = OutputStreamWriter(fileOutputStream)
+            outputWriter.write(getText())
+            outputWriter.close()
             true
         }catch(e: IOException){
+            println(e)
+            false
+        }
+    }
+    private fun deleteThisNote(noteName: String): Boolean{
+        return try{
+            deleteFile(noteName)
+            true
+        }catch(e: Exception){
             e.printStackTrace()
             false
         }
     }
-    private fun loadThisNote(noteName: String): String? {
-        try {
-            openFileInput(noteName).use {
-                val inputReader = InputStreamReader(it)
-                val noteRecievedFromFile = inputReader.read().toString()
-                return noteRecievedFromFile
-            }
+    private fun loadThisNote(noteName: String): String {
+        return try {
+            val fileInputStream = openFileInput(noteName)
+            val inputReader = InputStreamReader(fileInputStream)
+            val output = inputReader.readText()
+            output
         }catch(e: IOException){
-            return ""
+            ""
         }
     }
 }
