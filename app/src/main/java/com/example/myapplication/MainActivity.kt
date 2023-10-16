4f876edd4e7608f0c1b15c09fb02d4a04c23429c
Benton60 <145625002+Benton60@users.noreply.github.com>
2023-10-15 22:56:10 -0400

    created the database connection. added a way to upload your the notes to the database. there is a bug that create new users when it shouldnt need fixed immediately

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -6,27 +6,54 @@ import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.tasks.await
 
 class MainActivity : AppCompatActivity() {
+    private val  usersRef = Firebase.firestore.collection("users")
+    private val userName = "Admin"
+    private val password = "Admin"
+    private var userExists = "false"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         val edtFileName = findViewById<EditText>(R.id.edtFileName)
         val btnBigBoi = findViewById<Button>(R.id.btnBigBoi)
-
+        checkIfUserExists()
         showFiles()
         btnBigBoi.setOnClickListener {
-            Intent(this, OpenNoteActivity::class.java).also{
-                it.putExtra("EXTRA_FILENAME", edtFileName.text.toString())
+            Intent(this, OpenNoteActivity::class.java).also {
+                it.putExtra("EXTRA_FILENAME", edtFileName.text.toString() + ".note")
+                it.putExtra("EXTRA_USERNAME", userName)
+                it.putExtra("EXTRA_PASSWORD", password)
+                it.putExtra("EXTRA_USER-EXISTS", userExists)
                 startActivity(it)
             }
         }
     }
-
     override fun onResume() {
         super.onResume()
+        checkIfUserExists()
         showFiles()
+    }
+
+    private fun checkIfUserExists() = CoroutineScope (Dispatchers.IO).launch{
+        try {
+            val userQuery = usersRef
+                .whereEqualTo("user", userName)
+                .get()
+                .await()
+            if (userQuery.documents.isNotEmpty()) {
+                userExists = "true"
+            }
+        }catch(e: Exception){
+
+        }
     }
 
     private fun showFiles() {
@@ -41,8 +68,9 @@ class MainActivity : AppCompatActivity() {
             it.isFile
             it.canWrite()
             it.exists()
+            it.extension == "note"
         }?.forEach {
-            text += "\n" + it.name
+            text += "\n" + it.nameWithoutExtension
         }
         tvListFiles.text = text
     }
--- app/src/main/java/com/example/myapplication/OpenNoteActivity.kt
+++ app/src/main/java/com/example/myapplication/OpenNoteActivity.kt
@@ -5,22 +5,35 @@ import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.firestore.SetOptions
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.tasks.await
 import java.io.IOException
 import java.io.InputStreamReader
 import java.io.OutputStreamWriter
+import java.text.SimpleDateFormat
+import java.util.Date
 
 class OpenNoteActivity : AppCompatActivity() {
+    private val  usersRef = Firebase.firestore.collection("users")
+    private var userExists = "true"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_note)
         val btnSave = findViewById<Button>(R.id.btnSave)
         val btnDelete = findViewById<Button>(R.id.btnDelete)
+        userExists = intent.getStringExtra("EXTRA_USER-EXISTS").toString()
 
         setText(loadThisNote(getNoteName()))
 
         btnSave.setOnClickListener {
+            addDataToFireStore()
             if(saveThisNote(getNoteName())){
-                finish()
+
             }else{
                 setText("Failed")
             }
@@ -31,6 +44,58 @@ class OpenNoteActivity : AppCompatActivity() {
             finish()
         }
     }
+
+
+    private fun addDataToFireStore() = CoroutineScope(Dispatchers.IO).launch {
+        saveThisNote(getNoteName())
+        val db = Firebase.firestore
+        val notesToAddToDB = hashMapOf(
+            " " to " "
+        )
+        filesDir.listFiles()?.filter {
+            it.canRead()
+            it.isFile
+            it.canWrite()
+            it.exists()
+            it.extension == "note"
+        }?.forEach {
+            notesToAddToDB.put(it.name, loadThisNote(it.name))
+        }
+        notesToAddToDB.remove(" ")
+        if(userExists.equals("true")){
+            val user = usersRef
+                .whereEqualTo("user", intent.getStringExtra("EXTRA_USERNAME").toString())
+                .get()
+                .await()
+            for (document in user){
+                usersRef.document("lastTime").set (
+                    SimpleDateFormat("dd/M/yyyy hh:mm:ss").format(Date()),
+                    SetOptions.merge()
+                ).await()
+                usersRef.document("notes").set(
+                    notesToAddToDB,
+                    SetOptions.merge()
+                ).await()
+            }
+        }else {
+            val user = hashMapOf(
+                "user" to intent.getStringExtra("EXTRA_USERNAME").toString(),
+                "password" to intent.getStringExtra("EXTRA_PASSWORD").toString(),
+                "lastTime" to SimpleDateFormat("dd/M/yyyy hh:mm:ss").format(Date()),
+                "notes" to notesToAddToDB
+            )
+            //val toast = Toast.makeText(this, "Connect To The Internet", Toast.LENGTH_SHORT)
+            //toast.show()
+            db.collection("users")
+                .add(user)
+                .addOnSuccessListener {
+                    //val toast = Toast.makeText(this, "Saved", Toast.LENGTH_LONG)
+                    //toast.show()
+                }
+        }
+        finish()
+    }
+
     private fun getNoteName(): String {
          return intent.getStringExtra("EXTRA_FILENAME").toString()
     }
