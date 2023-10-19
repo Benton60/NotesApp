de37100a5668bbaa18b14f4bb8456b9fd7847a2c
Benton60 <145625002+Benton60@users.noreply.github.com>
2023-10-19 18:47:10 -0400

    Now io actually fixed that bug I mentioned earlier

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
@@ -32,6 +33,8 @@ class MainActivity : AppCompatActivity() {
                     it.putExtra("EXTRA_PASSWORD", password)
                     startActivity(it)
                 }
+            }else{
+                Toast.makeText(this, "Please Enter A File Name", Toast.LENGTH_SHORT).show()
             }
         }
     }
